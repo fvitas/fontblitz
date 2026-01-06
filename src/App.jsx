1a5f896cd82976f23f654bf018b141e2a5c1c5a0
Filip Vitas <filip.vitas.dev@gmail.com>
2026-01-06 10:40:11 +0100

    feat: add fuzzy search for all fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { Button } from '@/components/ui/button.jsx'
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog.jsx'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu.jsx'
 import { Input } from '@/components/ui/input.jsx'
+import { Label } from '@/components/ui/label.jsx'
 import { Tabs, TabsContent, TabsContents, TabsList, TabsTrigger } from '@/components/ui/motion-tabs.jsx'
+import { Slider } from '@/components/ui/slider.jsx'
 import { googleFonts } from '@/google-fonts.js'
+import Fuse from 'fuse.js'
 import { isEmpty } from 'lodash'
-import { SearchIcon, SlidersHorizontalIcon, Upload, XIcon } from 'lucide-react'
+import { SearchIcon, SettingsIcon, SlidersHorizontalIcon, Upload, XIcon } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import { Virtuoso } from 'react-virtuoso'
 import { twMerge } from 'tailwind-merge'
@@ -23,6 +33,7 @@ const popularSystemFonts = [
 ]
 const systemFonts = await chrome?.fontSettings?.getFontList()
 
+// TODO (filipv): check for duplicates in fonts
 const tabs = [
   {
     name: 'System Fonts',
@@ -45,6 +56,43 @@ const tabs = [
   },
 ]
 
+const fuse = new Fuse([...tabs.flatMap(tab => tab.fonts)], {
+  keys: ['name'],
+  threshold: 0.3,
+  minMatchCharLength: 2,
+  includeMatches: true,
+  includeScore: true,
+  isCaseSensitive: false,
+})
+
+function highlightText(match) {
+  let result = []
+  let lastIndex = 0
+
+  match.indices.forEach(([startIdx, endIdx]) => {
+    // Add text before the match
+    if (startIdx > lastIndex) {
+      result.push(match.value.substring(lastIndex, startIdx))
+    }
+
+    // Add the highlighted match
+    result.push(
+      <spans key={`highlight-${startIdx}`} className="bg-primary/50">
+        {match.value.substring(startIdx, endIdx + 1)}
+      </spans>,
+    )
+
+    lastIndex = endIdx + 1
+  })
+
+  // Add any remaining text after the last match
+  if (lastIndex < match.value.length) {
+    result.push(match.value.substring(lastIndex))
+  }
+
+  return result
+}
+
 function loadGoogleFont(fontFamily) {
   const googleFontId = `google-font-${fontFamily}`
   const fontLinkExists = document.getElementById(googleFontId)
@@ -60,7 +108,7 @@ function loadGoogleFont(fontFamily) {
   document.head.appendChild(link)
 }
 
-function FontButton({ font, fontType, isSelected, onPointerDown, onKeyDown }) {
+function FontButton({ font, fontType, isSelected, searchMatch, onPointerDown, onKeyDown }) {
   useEffect(() => {
     if (fontType === 'google') {
       loadGoogleFont(font.family)
@@ -79,7 +127,7 @@ function FontButton({ font, fontType, isSelected, onPointerDown, onKeyDown }) {
       style={{ fontFamily: font.family }}
       onPointerDown={onPointerDown}
       onKeyDown={onKeyDown}>
-      <span className="text-lg">{font.name}</span>
+      <span className="text-lg">{searchMatch ? highlightText(searchMatch) : font.name}</span>
     </Button>
   )
 }
@@ -91,6 +139,11 @@ export function App() {
   const [fontUrl, setFontUrl] = useState('')
   const [fontName, setFontName] = useState('')
   const [selectedFont, setSelectedFont] = useState(null)
+
+  let results = []
+  if (search.length >= 2) {
+    results = fuse.search(search)
+  }
 
   function handleFileUpload(event) {
     const file = event.target.files?.[0]
@@ -121,7 +174,7 @@ export function App() {
     }
   }
 
-  async function selectFont(font, type) {
+  async function selectFont(font, type = 'system') {
     setSelectedFont(font)
 
     // TODO (filipv): split and refactor this logic
@@ -238,7 +291,7 @@ export function App() {
   }
 
   return (
-    <div className="bg-card text-card-foreground m-0 flex w-96 flex-col gap-4 p-6 py-6">
+    <div className="bg-card text-card-foreground m-0 flex w-96 flex-col gap-4 p-4">
       {/*<div className="flex items-center justify-between">*/}
       {/*  <img src="/favicon/logo.svg" alt="Font Swapper logo" className="w-8" />*/}
 
@@ -249,8 +302,8 @@ export function App() {
       {/*  </Button>*/}
       {/*</div>*/}
 
-      <div className="flex gap-2">
-        <img src="/favicon/logo.svg" alt="Font Swapper logo" className="mr-2 w-8" />
+      <div className="flex">
+        <img src="/favicon/logo.svg" alt="Font Swapper logo" className="mr-4 w-8" />
 
         <div className="relative">
           <div className="text-muted-foreground pointer-events-none absolute inset-y-0 left-0 flex items-center justify-center pl-3 peer-disabled:opacity-50">
@@ -261,68 +314,186 @@ export function App() {
           <div className="flex gap-2">
             <Input
               id="search"
-              name="search"
+              name="font-switcher-search"
+              autocomplete="on"
               type="search"
               placeholder="Search..."
               className="peer px-9 [&::-webkit-search-cancel-button]:appearance-none [&::-webkit-search-decoration]:appearance-none [&::-webkit-search-results-button]:appearance-none [&::-webkit-search-results-decoration]:appearance-none"
               value={search}
               onChange={e => setSearch(e.target.value)}
             />
-            <Button variant="ghost" size="icon">
-              <SlidersHorizontalIcon />
-              <span className="sr-only">Settings</span>
-            </Button>
           </div>
+
+          {/*<Select defaultValue="All">*/}
+          {/*  <SelectTrigger>*/}
+          {/*    <SelectValue placeholder="Select category" />*/}
+          {/*  </SelectTrigger>*/}
+
+          {/*  <SelectContent>*/}
+          {/*    <SelectGroup>*/}
+          {/*      <SelectItem value="All">All</SelectItem>*/}
+          {/*      <SelectItem value="Sans">Sans</SelectItem>*/}
+          {/*      <SelectItem value="Serif">Serif</SelectItem>*/}
+          {/*      <SelectItem value="Slab">Slab</SelectItem>*/}
+          {/*      <SelectItem value="Display">Display</SelectItem>*/}
+          {/*      <SelectItem value="Handwritten">Handwritten</SelectItem>*/}
+          {/*    </SelectGroup>*/}
+          {/*  </SelectContent>*/}
+          {/*</Select>*/}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              {/*<InputGroupButton variant="ghost" className="!pr-1.5 text-xs">*/}
+              {/*  Search In... <ChevronDownIcon className="size-3" />*/}
+              {/*</InputGroupButton>*/}
+              <Button
+                variant="ghost"
+                size="icon-sm"
+                className="absolute inset-y-0.5 right-0.5 flex items-center justify-center peer-disabled:opacity-50">
+                <SlidersHorizontalIcon />
+                <span className="sr-only">Filter</span>
+              </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent align="end" className="[--radius:0.95rem]">
+              <DropdownMenuItem>All</DropdownMenuItem>
+              <DropdownMenuItem>Sans</DropdownMenuItem>
+              <DropdownMenuItem>Serif</DropdownMenuItem>
+              <DropdownMenuItem>Slab</DropdownMenuItem>
+              <DropdownMenuItem>Display</DropdownMenuItem>
+              <DropdownMenuItem>Handwritten</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
 
-        <Button variant="ghost" size="icon" onClick={() => window.close()}>
-          <XIcon />
-        </Button>
+        <div className="ml-4 flex gap-1">
+          <Dialog>
+            <form>
+              <DialogTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <SettingsIcon />
+                  <span className="sr-only">Settings</span>
+                </Button>
+              </DialogTrigger>
+
+              <DialogContent className="w-[350px]" showCloseButton={false}>
+                <DialogHeader className="text-left">
+                  <DialogTitle>Settings</DialogTitle>
+                </DialogHeader>
+
+                <div className="grid gap-4">
+                  <div className="grid gap-2">
+                    <Label htmlFor="name-3">Font size</Label>
+                    <div className="flex gap-2">
+                      <Slider defaultValue={[33]} max={100} step={1} />
+                      <Input id="name-3" name="name" type="number" value={33} className="w-20" />
+                    </div>
+                  </div>
+
+                  <div className="grid gap-2">
+                    <Label htmlFor="name-1">Letter spacing</Label>
+                    <div className="flex gap-2">
+                      <Slider defaultValue={[33]} max={100} step={1} />
+                      <Input id="name-1" name="name" type="number" value={33} className="w-20" />
+                    </div>
+                  </div>
+
+                  <div className="grid gap-2">
+                    <Label htmlFor="name-2">Line spacing</Label>
+                    <div className="flex gap-2">
+                      <Slider defaultValue={[33]} max={100} step={1} />
+                      <Input id="name-2" name="name" type="number" value={33} className="w-20" />
+                    </div>
+                  </div>
+                </div>
+
+                {/*<DialogFooter>*/}
+                {/*  <DialogClose asChild>*/}
+                {/*    <Button variant="outline">Cancel</Button>*/}
+                {/*  </DialogClose>*/}
+                {/*  <Button type="submit">Apply changes</Button>*/}
+                {/*</DialogFooter>*/}
+              </DialogContent>
+            </form>
+          </Dialog>
+
+          <Button variant="ghost" size="icon" onClick={() => window.close()}>
+            <XIcon />
+            <span className="sr-only">Close</span>
+          </Button>
+        </div>
       </div>
 
-      <Tabs defaultValue="system" className="gap-4" onValueChange={setActiveTab}>
-        <TabsList className="w-full">
-          {tabs.map(tab => (
-            <TabsTrigger key={tab.value} value={tab.value}>
-              {tab.name}
-            </TabsTrigger>
-          ))}
-        </TabsList>
+      {search.length >= 2 ? (
+        <div className="h-[300px] overflow-auto">
+          <Virtuoso
+            style={{ height: '100%' }}
+            data={results}
+            itemContent={(index, result) => (
+              <FontButton
+                key={'font-button-' + index}
+                font={result.item}
+                searchMatch={result?.matches?.at(0)}
+                isSelected={selectedFont?.name === result.item.name}
+                onPointerDown={event => {
+                  if (event.button === 0) {
+                    selectFont(result.item)
+                  }
+                }}
+                onKeyDown={event => {
+                  if (event.key === 'Enter' || event.key === ' ') {
+                    event.preventDefault()
+                    selectFont(result.item)
+                  }
+                }}
+              />
+            )}
+          />
+        </div>
+      ) : (
+        <Tabs defaultValue="system" className="gap-4" onValueChange={setActiveTab}>
+          <TabsList className="w-full">
+            {tabs.map(tab => (
+              <TabsTrigger key={tab.value} value={tab.value}>
+                {tab.name}
+              </TabsTrigger>
+            ))}
+          </TabsList>
 
-        <TabsContents className="bg-background h-full">
-          {tabs.map(tab => (
-            <TabsContent key={tab.value} value={tab.value} className="h-[300px] overflow-auto">
-              {isEmpty(tab.fonts) ? (
-                <div className="text-muted-foreground py-8 text-center">No fonts found</div>
-              ) : (
-                <Virtuoso
-                  style={{ height: '100%' }}
-                  data={tab?.fonts}
-                  itemContent={(index, font) => (
-                    <FontButton
-                      key={'font-button-' + index}
-                      font={font}
-                      fontType={tab.value}
-                      isSelected={selectedFont?.name === font.name}
-                      onPointerDown={event => {
-                        if (event.button === 0) {
-                          selectFont(font, tab.value)
-                        }
-                      }}
-                      onKeyDown={event => {
-                        if (event.key === 'Enter' || event.key === ' ') {
-                          event.preventDefault()
-                          selectFont(font, tab.value)
-                        }
-                      }}
-                    />
-                  )}
-                />
-              )}
-            </TabsContent>
-          ))}
-        </TabsContents>
-      </Tabs>
+          <TabsContents className="bg-background h-full">
+            {tabs.map(tab => (
+              <TabsContent key={tab.value} value={tab.value} className="h-[300px] overflow-auto">
+                {isEmpty(tab.fonts) ? (
+                  <div className="text-muted-foreground py-8 text-center">No fonts found</div>
+                ) : (
+                  <Virtuoso
+                    style={{ height: '100%' }}
+                    data={tab?.fonts}
+                    itemContent={(index, font) => (
+                      <FontButton
+                        key={'font-button-' + index}
+                        font={font}
+                        fontType={tab.value}
+                        isSelected={selectedFont?.name === font.name}
+                        onPointerDown={event => {
+                          if (event.button === 0) {
+                            selectFont(font, tab.value)
+                          }
+                        }}
+                        onKeyDown={event => {
+                          if (event.key === 'Enter' || event.key === ' ') {
+                            event.preventDefault()
+                            selectFont(font, tab.value)
+                          }
+                        }}
+                      />
+                    )}
+                  />
+                )}
+              </TabsContent>
+            ))}
+          </TabsContents>
+        </Tabs>
+      )}
 
       {activeTab === 'custom' && (
         <div className="border-border mb-4 space-y-3 rounded-md border p-4">
